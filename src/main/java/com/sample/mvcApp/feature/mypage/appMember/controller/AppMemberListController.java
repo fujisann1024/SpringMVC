bd8dbc49e0ee88860046a56ddd1785d0eea12acf
fujisann1024 <tehepero302@gmail.com>
2025-09-20 02:59:40 +0900

    名前規則の変更

--- src/main/java/com/sample/mvcApp/feature/mypage/appMember/controller/AppMemberListController.java
+++ src/main/java/com/sample/mvcApp/feature/mypage/appMember/controller/AppMemberListController.java
@@ -4,13 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
+/*
+ * 会員一覧Controller
+ */
 @Controller
-public class AppMemberController {
+public class AppMemberListController {
 	
 	@GetMapping("app-member")
 	public String GetAppMember(Model model) {
 		model.addAttribute("msg","タイムリーフ");
-		return "member/list";
+		return "appMember/list";
 	}
 }
