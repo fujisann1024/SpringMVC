94f4410cf73be0f188b166665c229ebab2935b94
fujisann1024 <tehepero302@gmail.com>
2025-10-30 05:00:34 +0900

    application層部分のコミット

--- src/main/java/com/sample/mvcApp/feature/mypage/task/application/output/TaskGroupWeekOutput.java
+++ src/main/java/com/sample/mvcApp/feature/mypage/task/application/output/TaskGroupWeekOutput.java
@@ -1,5 +1,8 @@
 package com.sample.mvcApp.feature.mypage.task.application.output;
 
+import java.time.LocalDate;
+import java.util.Map;
+
 public record TaskGroupWeekOutput(
-		Map<LocalDate,TaskGroupSummaryOutput>  taskGroupByWeekMap,
+		Map<LocalDate,TaskGroupSummaryOutput>  taskGroupByWeekMap
 		) { }
